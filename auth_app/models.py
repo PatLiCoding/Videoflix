4bc13dde7736c05b63eaab6caefd6ce280d968c3
Patricia Linne <patricia.linne@yahoo.de>
2026-07-24 09:30:48 +0200

    fix: fix FieldError, add Meta options and admin panel for custom User model

--- auth_app/models.py
+++ auth_app/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
-            raise ValueError("Die E-Mail-Adresse muss angegeben werden")
+            raise ValueError("The email address must be provided.")
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -20,12 +20,17 @@ class UserManager(BaseUserManager):
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    username = None
+    username = models.CharField(max_length=150, blank=True, null=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     objects = UserManager()
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "User"
+        ordering = ["email"]
 
     def __str__(self):
         return self.email
