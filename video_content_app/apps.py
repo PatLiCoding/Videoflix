8fe7117dc3d38b699ff34690800275b4c1793d6d
PatLiCoding <patricia.linne@yahoo.de>
2026-08-10 10:15:31 +0200

    feat: add Video model and initialize test structure

--- video_content_app/apps.py
+++ video_content_app/apps.py
@@ -4,5 +4,5 @@ from django.apps import AppConfig
 class VideoContentAppConfig(AppConfig):
     name = 'video_content_app'
 
-    # def ready(self):
-    #     from . import singals
+    def ready(self):
+        from . import singals
--- video_content_app/models.py
+++ video_content_app/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Video(models.Model):
+    VIDEO_CATEGORY = [
+        ('action', 'Action'),
+        ('anime', 'Anime'),
+        ('comedies', 'Comedies'),
+        ('crime', 'Crime'),
+        ('documentaries', 'Documentaries'),
+        ('drama', 'Drama'),
+        ('fantasy', 'Fantasy'),
+        ('horror', 'Horror'),
+        ('kids_family', 'Kids & Family'),
+        ('romance', 'Romance'),
+        ('sci_fi', 'Sci-Fi'),
+        ('thrillers', 'Thrillers'),
+    ]
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(
+        max_length=255, blank=True, default="Generating...")
+    description = models.TextField(blank=True)
+    thumbnail_url = models.URLField()
+    category = models.CharField(
+        max_length=20, choices=VIDEO_CATEGORY, default='action')
+    video_file = models.FileField(
+        upload_to="videos/originals/", blank=True, null=True)
+    hls_path = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        """Meta options for the Video model."""
+        ordering = ["-created_at"]
+        verbose_name = "Video"
+        verbose_name_plural = "Videos"
+
+    def __str__(self):
+        """
+        Returns a string representation of the Video.
+
+        Returns:
+            str: The title of the video.
+        """
+        return self.title
